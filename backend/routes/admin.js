0de52aac7b764615cde2e37df6bb6566c6fa1510
Combrero <fchel.sea.v@gmail.com>
2024-12-24 09:47:33 +0700

    tidy up

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -11,22 +11,11 @@ const {
 
 const router = express.Router();
 
-// Admin landing page (fetch all crowdfunds)
 router.get('/admin', getAllCrowdfunds);
-
-// Admin detail Crowdfund
 router.get('/admin/:id', isAdmin, getCrowdfundDetails);
-
-// Admin create Crowdfund
 router.post('/admin/create', createCrowdfund);
-
-// Admin edit Crowdfund
 router.put('/admin/:id/edit', isAdmin, editCrowdfund);
-
-// Delete crowdfund
 router.delete('/admin/:id', isAdmin, deleteCrowdfund);
-
-// Delete comment
 router.delete('/admin/comment/:id', isAdmin, deleteComment);
 
 module.exports = router;
